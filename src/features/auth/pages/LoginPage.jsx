f3260e01cce6fa4ca49779a7fb2ac21eb1a7354e
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-07-19 10:38:04 +0200

    new_tailwind

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { LoginForm } from '../component/LoginForm'
+import { Header } from '../../../shared/components/Header'
 
 export const LoginPage = () => {
   return (
-    <div><LoginForm/></div>
+    <>
+     <div><LoginForm/></div>
+      <div><Header/></div>
+    </>
+   
   )
 }
