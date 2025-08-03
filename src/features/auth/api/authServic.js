fd257f78331feb7c902fe920bca263a48ffb040b
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-03 16:00:33 +0200

    fix signup servic

--- src/features/auth/api/authServic.js
+++ src/features/auth/api/authServic.js
@@ -1,32 +1,29 @@
-import axios from 'axios'
-import { data } from 'react-router';
+import axios from "axios";
 
-export const loginservice = async(email, password) => {
-  const res = await axios.post(" https://api.escuelajs.co/api/v1/auth/login",
-    {
-  "email": email,
-  "password": password
-}
-  ) 
-  const data = res.data 
+export const loginservice = async (email, password) => {
+  const res = await axios.post(" https://api.escuelajs.co/api/v1/auth/login", {
+    email: email,
+    password: password,
+  });
+  const data = res.data;
   console.log(data);
-  
-  
-}
+};
 
+export const signUpService = async (name, email, password) => {
+  try {
+    const res = await axios.post(
+      "https://api.escuelajs.co/api/v1/users/", // get and post
+      {
+        name,
+        email,
+        password,
+        avatar: "https://picsum.photos/800",
+      }
+    );
+    const data = res.data;
+    console.log("Signup success:", data);
 
-export const signUpService = async(name,email,password) => {
-  const res = await axios.get("https://api.escuelajs.co/api/v1/users/", // get and post
-    {
-      "name":name,
-      "email":email,
-      "password":password,
-      " avatar":"https://picsum.photos/800"
-
-    }
-  )
-   const data = res.data 
- console.log("Signup success:", data);
- 
-}
-
+  } catch (err) {
+    console.error("Signup error:", err.response?.data || err.message);
+  }
+};
